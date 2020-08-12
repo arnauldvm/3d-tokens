a78faf6344f416c1d757e258f5c4455b4b73fa77
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2020-08-12 19:16:52 +0200

    add(chore): env config supports multiple platforms

--- env.source.sh
+++ env.source.sh
@@ -1,1 +1,18 @@
-OPENSCAD_PATH="$HOME/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD"
+
+OPENSCAD_PATH_CANDIDATES=()
+# Mac OS
+OPENSCAD_PATH_CANDIDATES+=("/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD")
+OPENSCAD_PATH_CANDIDATES+=("$HOME/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD")
+# Windows
+OPENSCAD_PATH_CANDIDATES+=("/mnt/c/Program Files/OpenSCAD/openscad.exe")
+# Linux
+OPENSCAD_PATH_CANDIDATES+=("/usr/bin/openscad")
+OPENSCAD_PATH_CANDIDATES+=("/bin/openscad")
+# Default
+OPENSCAD_PATH_CANDIDATES+=("openscad")
+
+OPENSCAD_PATH=$(
+for f in "${OPENSCAD_PATH_CANDIDATES[@]}"; do
+    command -v "$f" &> /dev/null && { echo "$f"; break; }
+done
+)
